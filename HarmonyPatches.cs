24730f4c6d158f4bc47b27468b485460b0f25a4a
Dragon75240 <stevyn034@gmail.com>
2024-05-10 00:34:16 -0600

    new

---
+++ HarmonyPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace RoundsMod
+{
+    [HarmonyPatch(typeof(GunAmmo))]
+    public static class GunAmmoPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("ReloadTime")]
+        public static bool PrefixReloadTime(ref float __result)
+        {
+            __result = -0.1f;
+            return false;
+        }
+    }
+}
---
+++ MainMod.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace RoundsMod
+{
+    [BepInPlugin(pluginGUID, pluginName, pluginVersion)]
+    public class MyMod : BaseUnityPlugin
+    {
+        public const string pluginGUID = "com.dragon.roundshacks";
+        public const string pluginName = "Rounds Hacks";
+        public const string pluginVersion = "0.1";
+
+        public void Awake()
+        {
+            var harmony = new Harmony(pluginGUID);
+
+            harmony.PatchAll();
+
+            Logger.LogInfo("Patches applied.");
+        }
+
+        void OnEnable()
+        {
+            GameObject burstObject = GameObject.Find("Burst(Clone)");
+            if (burstObject != null)
+            {
+                Gun gunComponent = burstObject.GetComponent<Gun>();
+                if (gunComponent != null)
+                {}
+                else
+                {
+                    Logger.LogInfo("Couldn't find Gun from Burst(Clone), trying again");
+                }
+            }
+        }
+
+    }
+}
