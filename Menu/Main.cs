c0cfb63212860c3e0b1361d4220b0dcf4e9a859d
Dragon <66880163+Dragon75240@users.noreply.github.com>
2024-06-24 17:53:03 -0600

    added testing panel

---
+++ Menu/Main.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace RoundsMod.Menu
+{
+    public class MainModMenuHandler
+    {
+        public void AddMenu()
+        {
+            GameObject canvasObject = new GameObject("Canvas");
+            Canvas canvas = canvasObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+            Vector2 referenceSize = new Vector2();
+            referenceSize.x = 1920;
+            referenceSize.y = 1080;
+
+            CanvasScaler scaler = canvasObject.AddComponent<CanvasScaler>();
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            scaler.referenceResolution = referenceSize;
+
+            GameObject panelObject = new GameObject("Panel");
+            RectTransform rectTransform = panelObject.AddComponent<RectTransform>();
+            Image image = panelObject.AddComponent<Image>();
+
+            rectTransform.SetParent(canvas.transform);
+            rectTransform.anchoredPosition = Vector2.zero;
+            rectTransform.sizeDelta = new Vector2(200, 100);
+
+            image.color = Color.black;
+        }
+
+    }
+}
---
+++ Patches/AddMenu.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RoundsMod.Menu;
+
+namespace RoundsMod.Patches
+{
+    [HarmonyPatch(typeof(Map))]
+    public static class AddMenuPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("Start")]
+        public static void Patch()
+        {
+            MainModMenuHandler handler = new MainModMenuHandler();
+
+            handler.AddMenu();
+        }
+    }
+}
